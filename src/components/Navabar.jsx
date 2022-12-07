0e56d9dd695088bcafa40424901ea0f89fdf0a5f
Soe Sandar Win <98808636+alyssaackerman2001@users.noreply.github.com>
2022-12-07 15:52:36 +0630

    Create the navbar for thepage

---
+++ src/components/Navabar.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../index.css';
+
+class Navbar extends Component {
+ render() { 
+ 
+  return (
+   <div className="navbar">
+    <span className="logo">Math Magicians</span>
+    <ul>
+     <li>
+      <Link to='/'>Home</Link>
+     </li>
+     <li>
+      <Link to='/calculator'>Calculator</Link>
+     </li>
+     <li>
+      <Link to='/quotes'>Quotes</Link>
+     </li>
+    </ul>
+   </div>
+  );
+ }
+}
+ 
+export default Navbar;
