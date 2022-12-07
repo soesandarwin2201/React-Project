3d8b491be302959ed0b9653c5baf017cbd46428f
Soe Sandar Win <98808636+alyssaackerman2001@users.noreply.github.com>
2022-12-07 15:53:37 +0630

    Make the functionality work with react route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Claculator from './components/Calculator';
+import Home from './components/Home';
+import Quotes from './components/Quotes';
+import Navbar from './components/Navabar';
+import './index.css';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
   render() {
     return (
-      <Claculator />
+      <>
+        <div className="spa">
+          <Navbar />
+          <Routes>
+            <Route path="/calculator" element={<Claculator />} />
+            <Route path="/quotes" element={<Quotes />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </>
     );
   }
 }
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -21,82 +21,89 @@ class Claculator extends React.Component {
   render() {
     const { total, next, operation } = this.state;
     return (
-      <div className='cal-grid'>
-        <div className='cal-result'>
-          <p>{total}</p>
-          <p>{operation}</p>
-          <p>{next}</p>
+      <>
+        <div className='app'>
+          <div className='calculator-title'>
+            <h1>Let's do some math!</h1>
+          </div>
+          <div className='cal-grid'>
+            <div className='cal-result'>
+              <p>{total}</p>
+              <p>{operation}</p>
+              <p>{next}</p>
+            </div>
+            <div className='cal-btn-body'>
+              <div className='row'>
+                <div onClick={this.eventHandler} className='col-1 col'>
+                  AC
+                </div>
+                <div onClick={this.eventHandler} className='col-2 col'>
+                  +/-
+                </div>
+                <div onClick={this.eventHandler} className='col-3 col'>
+                  %
+                </div>
+                <div onClick={this.eventHandler} className='col-4 col'>
+                  ÷
+                </div>
+              </div>
+              <div className='row'>
+                <div onClick={this.eventHandler} className='col-1 col'>
+                  7
+                </div>
+                <div onClick={this.eventHandler} className='col-2 col'>
+                  8
+                </div>
+                <div onClick={this.eventHandler} className='col-3 col'>
+                  9
+                </div>
+                <div onClick={this.eventHandler} className='col-4 col'>
+                  x
+                </div>
+              </div>
+              <div className='row'>
+                <div onClick={this.eventHandler} className='col-1 col'>
+                  4
+                </div>
+                <div onClick={this.eventHandler} className='col-2 col'>
+                  5
+                </div>
+                <div onClick={this.eventHandler} className='col-3 col'>
+                  6
+                </div>
+                <div onClick={this.eventHandler} className='col-4 col'>
+                  -
+                </div>
+              </div>
+              <div className='row'>
+                <div onClick={this.eventHandler} className='col-1 col'>
+                  1
+                </div>
+                <div onClick={this.eventHandler} className='col-2 col'>
+                  2
+                </div>
+                <div onClick={this.eventHandler} className='col-3 col'>
+                  3
+                </div>
+                <div onClick={this.eventHandler} className='col-4 col'>
+                  +
+                </div>
+              </div>
+              <div className='row'>
+                <div onClick={this.eventHandler} className='col-1 span col'>
+                  0
+                </div>
+                <div onClick={this.eventHandler} className='col-3 col'>
+                  .
+                </div>
+                <div onClick={this.eventHandler} className='col-4 col'>
+                  =
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className='cal-btn-body'>
-          <div className='row'>
-            <div onClick={this.eventHandler} className='col-1 col'>
-              AC
-            </div>
-            <div onClick={this.eventHandler} className='col-2 col'>
-              +/-
-            </div>
-            <div onClick={this.eventHandler} className='col-3 col'>
-              %
-            </div>
-            <div onClick={this.eventHandler} className='col-4 col'>
-              ÷
-            </div>
-          </div>
-          <div className='row'>
-            <div onClick={this.eventHandler} className='col-1 col'>
-              7
-            </div>
-            <div onClick={this.eventHandler} className='col-2 col'>
-              8
-            </div>
-            <div onClick={this.eventHandler} className='col-3 col'>
-              9
-            </div>
-            <div onClick={this.eventHandler} className='col-4 col'>
-              x
-            </div>
-          </div>
-          <div className='row'>
-            <div onClick={this.eventHandler} className='col-1 col'>
-              4
-            </div>
-            <div onClick={this.eventHandler} className='col-2 col'>
-              5
-            </div>
-            <div onClick={this.eventHandler} className='col-3 col'>
-              6
-            </div>
-            <div onClick={this.eventHandler} className='col-4 col'>
-              -
-            </div>
-          </div>
-          <div className='row'>
-            <div onClick={this.eventHandler} className='col-1 col'>
-              1
-            </div>
-            <div onClick={this.eventHandler} className='col-2 col'>
-              2
-            </div>
-            <div onClick={this.eventHandler} className='col-3 col'>
-              3
-            </div>
-            <div onClick={this.eventHandler} className='col-4 col'>
-              +
-            </div>
-          </div>
-          <div className='row'>
-            <div onClick={this.eventHandler} className='col-1 span col'>
-              0
-            </div>
-            <div onClick={this.eventHandler} className='col-3 col'>
-              .
-            </div>
-            <div onClick={this.eventHandler} className='col-4 col'>
-              =
-            </div>
-          </div>
-        </div>
-      </div>
+      </>
     );
   }
 }
