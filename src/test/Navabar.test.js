1c2ccce0c0245e35a330c91f9c10b21c3c1d96e5
Soe Sandar Win <98808636+alyssaackerman2001@users.noreply.github.com>
2022-12-08 14:05:58 +0630

    Fix the bug for navbar.test.js

--- src/test/Navabar.test.js
+++ src/test/Navabar.test.js
@@ -2,14 +2,15 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from '../components/Navabar';
+import renderer from 'react-test-renderer';
 
 describe('navbar render correctly', () => {
   it('render correctly', () => {
-    const { tree } = render(
-      <BrowserRouter>
-        <Navbar />
-      </BrowserRouter>,
-    );
+   const tree = renderer.create(
+    <BrowserRouter>
+    <Navbar />
+    </BrowserRouter>
+   ).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
