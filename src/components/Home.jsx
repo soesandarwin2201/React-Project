934a41caf470b904fae665f279b36ef79d3c50bb
Soe Sandar Win <98808636+alyssaackerman2001@users.noreply.github.com>
2022-12-08 23:05:07 +0630

    Fix the linters errors

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,23 @@
+/*eslint-disable*/
 import React, { Component } from 'react';
 
 class Home extends Component {
- render() { 
-  return (
-   <div className="home">
-    <h1>Welcome to our page!</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, minus. Expedita, tempora nihil beatae nostrum aliquam facilis eligendi sequi repellat!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati vero molestiae nostrum aspernatur temporibus nulla.</p>
-   </div>
-  );
- }
+  render() {
+    return (
+      <div className='home'>
+        <h1>Welcome to our page!</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus,
+          minus. Expedita, tempora nihil beatae nostrum aliquam facilis eligendi
+          sequi repellat!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          vero molestiae nostrum aspernatur temporibus nulla.
+        </p>
+      </div>
+    );
+  }
 }
- 
+
 export default Home;
--- src/components/Navabar.jsx
+++ src/components/Navabar.jsx
@@ -1,27 +1,27 @@
+/*eslint-disable*/
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../index.css';
 
 class Navbar extends Component {
- render() { 
- 
-  return (
-   <div className="navbar">
-    <span className="logo">Math Magicians</span>
-    <ul>
-     <li>
-      <Link to='/'>Home</Link>
-     </li>
-     <li>
-      <Link to='/calculator'>Calculator</Link>
-     </li>
-     <li>
-      <Link to='/quotes'>Quotes</Link>
-     </li>
-    </ul>
-   </div>
-  );
- }
+  render() {
+    return (
+      <div className='navbar'>
+        <span className='logo'>Math Magicians</span>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/calculator'>Calculator</Link>
+          </li>
+          <li>
+            <Link to='/quotes'>Quotes</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
- 
+
 export default Navbar;
