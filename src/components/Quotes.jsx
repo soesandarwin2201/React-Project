339c8c599177dfba92af27a2359b24f1bc4f731a
Soe Sandar Win <98808636+alyssaackerman2001@users.noreply.github.com>
2022-12-08 22:54:19 +0630

    Check the linters error

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,14 +1,18 @@
-import React, { Component } from 'react'
+/*eslint-disable*/
+import React, { PureComponent } from 'react';
 
-class Quotes extends Component {
- render() { 
-  return (
-   <div className="quote">
-    <blockquote>Mathematics is not about numbers, equations, comutation or algorithms: It is about understanding </blockquote>
-    <cite>WIlliam Paul Thurston</cite>
-   </div>
-  );
- }
+class Quotes extends PureComponent {
+  render() {
+    return (
+      <div className='quote'>
+        <blockquote>
+          Mathematics is not about numbers, equations, comutation or algorithms:
+          It is about understanding{' '}
+        </blockquote>
+        <cite>WIlliam Paul Thurston</cite>
+      </div>
+    );
+  }
 }
- 
+
 export default Quotes;
